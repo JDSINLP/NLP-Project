e479a2f881538b227089ccfb7598a51562113f7f
Dev Gurung <dev.gurung141@gmail.com>
2023-03-08 23:21:27 -0600

    revised

--- explore.py
+++ explore.py
@@ -28,52 +28,57 @@ df=pd.read_json('data2.json')
 
 #Question 1
 def viz_most_common_unigrams(words):
-    '''takes in words, get top 20 unigram words, plot bar graph of top 20 unigram words'''
+    '''takes in words, get top 10 unigram words, plot bar graph of top 5 unigram words'''
     
     words_unigrams = pd.Series(nltk.ngrams(words.split(), 1))
-    top_20_words = words_unigrams.value_counts().head(20)
-    top_20_words.sort_values().plot.barh(color='pink', width=.9, figsize=(10, 6))
+    top_5_words = words_unigrams.value_counts().head(5)
+    ax = top_5_words.sort_values().plot.barh(color='pink', width=.9, figsize=(10, 6))
 
-    plt.title('20 Most frequently occuring words unigrams')
+    plt.title('5 Most frequently occuring words unigrams')
     plt.ylabel('unigram')
     plt.xlabel('# Occurances')
+    ax.spines[['right', 'top']].set_visible(False)
+
 
     # make the labels pretty
     ticks, _ = plt.yticks()
-    labels = top_20_words.reset_index()['index'].apply(lambda t: t[0])
+    labels = top_5_words.sort_values().reset_index()['index'].apply(lambda t: t[0])
     _ = plt.yticks(ticks, labels)
     
 
 def viz_most_common_bigrams(words):
-    '''takes in words, get top 20 bigram words, plot bar graph of top 20 bigram words'''
+    '''takes in words, get top 10 bigram words, plot bar graph of top 5 bigram words'''
     
     words_bigrams = pd.Series(nltk.ngrams(words.split(), 2))
-    top_20_words = words_bigrams.value_counts().head(20)
-    top_20_words.sort_values().plot.barh(color='pink', width=.9, figsize=(10, 6))
+    top_5_words = words_bigrams.value_counts().head(5)
+    ax = top_5_words.sort_values().plot.barh(color='pink', width=.9, figsize=(10, 6))
 
-    plt.title('20 Most frequently occuring words bigrams')
+    plt.title('5 Most frequently occuring words bigrams')
     plt.ylabel('Bigram')
     plt.xlabel('# Occurances')
+    ax.spines[['right', 'top']].set_visible(False)
 
     # make the labels pretty
     ticks, _ = plt.yticks()
-    labels = top_20_words.reset_index()['index'].apply(lambda t: t[0] + ' ' + t[1])
+    labels = top_5_words.sort_values().reset_index()['index'].apply(lambda t: t[0] + ' ' + t[1])
     _ = plt.yticks(ticks, labels)
     
 def viz_most_common_trigrams(words):
-    '''takes in words, get top 20 bigram words, plot bar graph of top 20 bigram words'''
+    '''takes in words, get top 5 trigram words, plot bar graph of top 5 bigram words'''
     
     words_trigrams = pd.Series(nltk.ngrams(words.split(), 3))
-    top_20_words = words_trigrams.value_counts().head(20)
-    top_20_words.sort_values().plot.barh(color='pink', width=.9, figsize=(10, 6))
+    top_5_words = words_trigrams.value_counts().head(5)
+    ax = top_5_words.sort_values().plot.barh(color='pink', width=.9, figsize=(10, 6))
 
-    plt.title('20 Most frequently occuring words trigrams')
+    plt.title('5 Most frequently occuring words trigrams')
     plt.ylabel('Trigram')
     plt.xlabel('# Occurances')
+    ax.spines[['right', 'top']].set_visible(False)
+
 
     # make the labels pretty
     ticks, _ = plt.yticks()
-    labels = top_20_words.reset_index()['index'].apply(lambda t: t[0] + ' ' + t[1] + ' ' + t[2])
+    labels = top_5_words.sort_values().reset_index()['index'].apply(lambda t: t[0] + ' ' + t[1] + ' ' + t[2])
     _ = plt.yticks(ticks, labels)
 
 def Q1(train):
@@ -90,13 +95,14 @@ def Q1(train):
 def viz_length_content(train):
     '''takes in a dataframe, plot a bar graph of a lenth of content by programming language'''
     
-    ax = sns.barplot(data=train, x='language',y='length' ,order=[ 'C++','Python' ,'Java', 'JavaScript'])
+    ax = sns.barplot(data=train, x='length',y='language', orient='h' ,order=['JavaScript', 'Java', 'Python', 'C++'])
     avg_length = train.length.mean()
-    plt.axhline(avg_length , label="Avg Length", color='yellow')
+    plt.axvline(avg_length , label="Avg Length", color='purple')
     plt.legend()
-    plt.xlabel('Programming Language')
-    plt.ylabel('Length')
-    plt.ylim(0,12000)
+    ax.legend(loc='upper left', bbox_to_anchor=(1, 1))
+    plt.ylabel('Programming Language')
+    plt.xlabel('Length')
+    plt.xlim(0,12000)
     plt.title('Variation of length of README by programming language')
     ax.spines[['right', 'top']].set_visible(False)
     plt.show()
@@ -106,7 +112,7 @@ def viz_count_unique(train):
     '''takes in a dataframe, plot a bar graph of a number of unique words of content by programming language'''
 
     
-    ax = sns.barplot(data=train, x='unique',y='language' ,orient ='h', order=[  'Java','Python','JavaScript','C++'])
+    ax = sns.barplot(data=train, x='unique',y='language' ,orient='h', order=[  'Java','Python','JavaScript','C++'])
     plt.ylabel('Programming Language')
     plt.xlabel('Length')
 #     plt.xlim(30,40)
